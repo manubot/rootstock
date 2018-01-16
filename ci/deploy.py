5bd95bc3e64d54a34c167c20a3e964a9d61489d8
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2018-01-16 13:17:03 -0500

    Structure webpage for commit directories
    Refs https://github.com/greenelab/manubot-rootstock/issues/96

---
+++ ci/deploy.py
@@ -0,0 +1,41 @@
+import os
+import pathlib
+import shutil
+
+commit = os.environ.get('TRAVIS_COMMIT', 'local')
+print(f'TRAVIS_COMMIT={commit}')
+
+# Create webpage/v/commit directory
+version_directory = pathlib.Path('webpage/v')
+version_directory.mkdir(exist_ok=True)
+commit_directory = version_directory.joinpath(commit)
+commit_directory.mkdir(exist_ok=True)
+
+# Symlink webpage/v/latest to point to webpage/v/commit
+latest_directory = version_directory.joinpath('latest')
+if latest_directory.is_symlink():
+    latest_directory.unlink()
+latest_directory.symlink_to(commit, target_is_directory=True)
+
+# Copy content/images to webpage/v/commit/images
+dst = commit_directory.joinpath('images')
+if dst.is_dir():
+    shutil.rmtree(dst)
+shutil.copytree(
+    src=pathlib.Path('content/images'),
+    dst=dst,
+)
+
+# Copy output files to to webpage/v/commit/
+output_directory = pathlib.Path('output')
+for name in 'manuscript.html', 'manuscript.pdf':
+    shutil.copy2(
+        src=output_directory.joinpath(name),
+        dst=commit_directory.joinpath(name),
+    )
+
+# Copy webpage/github-pandoc.css to to webpage/v/commit/github-pandoc.css
+shutil.copy2(
+    src=pathlib.Path('webpage/github-pandoc.css'),
+    dst=commit_directory.joinpath('github-pandoc.css'),
+)
