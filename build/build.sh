d3f5101683be600174093c91ea64ede6fb9a5df3
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-03-09 10:38:51 -0500

    fix accidental deletion of docx & pdf & analytics

--- build/build.sh
+++ build/build.sh
@@ -45,7 +45,50 @@ pandoc --verbose \
   --mathjax \
   --variable math="" \
   --include-after-body=build/plugins/math.html \
+  --include-after-body=build/plugins/annotations.html \
+  --include-after-body=build/plugins/analytics.html \
   --output=output/manuscript.html \
   $INPUT_PATH
+
+# Create PDF output (unless BUILD_PDF environment variable equals "false")
+if [ "$BUILD_PDF" != "false" ]; then
+  echo "Exporting PDF manuscript"
+  if [ -L images ]; then rm images; fi  # if images is a symlink, remove it
+  ln -s content/images
+  pandoc \
+    --from=markdown \
+    --to=html5 \
+    --pdf-engine=weasyprint \
+    --pdf-engine-opt=--presentational-hints \
+    --filter=pandoc-fignos \
+    --filter=pandoc-eqnos \
+    --filter=pandoc-tablenos \
+    --bibliography=$BIBLIOGRAPHY_PATH \
+    --csl=$CSL_PATH \
+    --metadata link-citations=true \
+    --webtex=https://latex.codecogs.com/svg.latex? \
+    --include-after-body=build/themes/default.html \
+    --output=output/manuscript.pdf \
+    $INPUT_PATH
+  rm images
+fi
+
+# Create DOCX output (if BUILD_DOCX environment variable equals "true")
+if [ "$BUILD_DOCX" = "true" ]; then
+  echo "Exporting Word Docx manuscript"
+  pandoc --verbose \
+    --from=markdown \
+    --to=docx \
+    --filter=pandoc-fignos \
+    --filter=pandoc-eqnos \
+    --filter=pandoc-tablenos \
+    --bibliography=$BIBLIOGRAPHY_PATH \
+    --csl=$CSL_PATH \
+    --metadata link-citations=true \
+    --reference-doc=build/themes/default.docx \
+    --resource-path=.:content \
+    --output=output/manuscript.docx \
+    $INPUT_PATH
+fi
 
 echo "Build complete"
