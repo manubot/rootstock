383df98fe479d6a29ff57d321fb4bc48d7d54ff9
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-04-19 11:13:10 -0400

    Update cp -r to -R for macOS

--- build/build.sh
+++ build/build.sh
@@ -80,7 +80,7 @@ fi
 if [ "$BUILD_PDF" != "false" ] && [ -n "$DOCKER_EXISTS" ]; then
   echo "Exporting PDF manuscript using Docker + Athena"
   if [ -d output/images ]; then rm -rf output/images; fi  # if images is a directory, remove it
-  cp -r -L content/images output/
+  cp -R -L content/images output/
   docker run \
     --rm \
     --volume `pwd`/output:/converted/ \
