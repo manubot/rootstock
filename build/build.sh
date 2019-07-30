b45fabc204cfc09c0f6561ecc2a6aeea05176156
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-07-30 11:26:45 -0400

    Conditionally set MANUBOT_ATHENAPDF_DELAY
    Default to --delay=5000 (5 seconds) for CI builds, where ensuring
    rendering is complete is crucial and Travis instances may have high load.
    Default to  --delay=1100 (1.1 seconds) for local builds, where there is
    a greater chance that users are running the command in real-time.
    
    The MANUBOT_ATHENAPDF_DELAY environment variable can now
    be set to override default values.

--- build/build.sh
+++ build/build.sh
@@ -60,6 +60,7 @@ pandoc --verbose \
 DOCKER_EXISTS="$(command -v docker || true)"
 
 # Create PDF output (unless BUILD_PDF environment variable equals "false")
+# If Docker is not available, use WeasyPrint to create PDF
 if [ "${BUILD_PDF:-}" != "false" ] && [ -z "$DOCKER_EXISTS" ]; then
   echo >&2 "Exporting PDF manuscript using WeasyPrint"
   if [ -L images ]; then rm images; fi  # if images is a symlink, remove it
@@ -82,9 +83,12 @@ if [ "${BUILD_PDF:-}" != "false" ] && [ -z "$DOCKER_EXISTS" ]; then
   rm images
 fi
 
-# Create PDF output (unless BUILD_PDF environment variable equals "false")
+# If Docker is available, use athenapdf to create PDF
 if [ "${BUILD_PDF:-}" != "false" ] && [ -n "$DOCKER_EXISTS" ]; then
   echo >&2 "Exporting PDF manuscript using Docker + Athena"
+  if [ "${CI:-}" = "true" ]; then
+    echo >&2 "Continuous integration build detected. Setting athenapdf --delay=${MANUBOT_ATHENAPDF_DELAY:=5000}"
+  fi
   if [ -d output/images ]; then rm -rf output/images; fi  # if images is a directory, remove it
   cp -R -L content/images output/
   docker run \
@@ -94,7 +98,7 @@ if [ "${BUILD_PDF:-}" != "false" ] && [ -n "$DOCKER_EXISTS" ]; then
     --security-opt=seccomp:unconfined \
     arachnysdocker/athenapdf:2.16.0 \
     athenapdf \
-    --delay=5000 \
+    --delay=${MANUBOT_ATHENAPDF_DELAY:-1100} \
     manuscript.html manuscript.pdf
   rm -rf output/images
 fi
