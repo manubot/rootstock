62755dee1cabf0aa9d428a811ec9a78232b37780
Anthony Gitter <gitter@biostat.wisc.edu>
2020-05-05 07:59:14 -0500

    Move and ignore spellcheck filter

--- ci/install-spellcheck.sh
+++ ci/install-spellcheck.sh
@@ -9,3 +9,4 @@ set -o errexit \
 sudo apt-get update -y
 sudo apt-get install -y aspell aspell-en
 wget https://raw.githubusercontent.com/pandoc/lua-filters/13c3fa7e97206413609a48a82575cb43137e037f/spellcheck/spellcheck.lua
+mv spellcheck.lua build/assets/spellcheck.lua
