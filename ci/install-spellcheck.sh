ef82887fffda3b5ea8831b84d94064cdc4a1c7ae
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2024-12-20 09:40:54 -0500

    fix appveyor error
    From https://ci.appveyor.com/project/manubot/rootstock/builds/51206619#L577
    E: Repository 'http://ppa.launchpad.net/rabbitmq/rabbitmq-erlang/ubuntu jammy InRelease' changed its 'Label' value from 'Latest (26.x) Erlang packages for Ubuntu and Debian' to 'Modern Erlang and Elixir packages for Ubuntu'
    N: This must be accepted explicitly before updates for this repository can be applied. See apt-secure(8) manpage for details.

--- ci/install-spellcheck.sh
+++ ci/install-spellcheck.sh
@@ -6,7 +6,8 @@
 set -o errexit \
     -o pipefail
 
-sudo apt-get update --yes
+# --allow-releaseinfo-change for error on appveyor like "Repository ... changed its 'Label' value"
+sudo apt-get update --yes --allow-releaseinfo-change
 sudo apt-get install --yes aspell aspell-en
 wget --directory-prefix=build/pandoc/filters \
   https://github.com/pandoc/lua-filters/raw/13c3fa7e97206413609a48a82575cb43137e037f/spellcheck/spellcheck.lua
